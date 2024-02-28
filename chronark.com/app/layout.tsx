5d358f7dba0de9e8d24a412c04445bce22c5058b
Vusal Ismayilov <woosal@protonmail.com>
2024-02-28 23:59:19 +0300

    Production Release [1.0]

---
+++ chronark.com/app/layout.tsx
@@ -0,0 +1,76 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+  title: {
+    default: "woosal.net",
+    template: "%s | woosal.net",
+  },
+  description: "currently building sorsor, uniwave.",
+  openGraph: {
+    title: "woosal.net",
+    description:
+      "currently building sorsor, uniwave.",
+    url: "https://woosal.net",
+    siteName: "woosal.net",
+    images: [
+      {
+        url: "https://s3.eu-central-1.amazonaws.com/woosal.net/Image+1746x894.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "woosal1337",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon-32x32.png",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
